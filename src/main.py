6ca46a0c204ef0de7d9bea9f67fe731461dce9ac
psochando <321pablosan@gmail.com>
2023-09-18 20:40:39 +0200

    .

--- src/main.py
+++
@@ -1,6 +0,0 @@
-'''
-Aqui voy a meter cosas como:
-- La funcion try_without, pero sin las graficas. Las graficas iran en el archivo visualizaciones
--  
-- Las funciones para utilizar el modelo en nuevas predicciones (eso mas adelante)
-'''
