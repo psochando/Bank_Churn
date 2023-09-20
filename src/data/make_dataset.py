e4dc39a6df4f9e1f8f1c3837a16cc871707c736e
psochando <321pablosan@gmail.com>
2023-09-20 11:13:08 +0200

    Automatizaciones y mejoras en make_dataset y analysis

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,33 +2,38 @@ import sys
 # Agregamos la ruta al directorio principal 'banck_CHURN' al PYTHONPATH para luego poder acceder a los modulos con rutas absolutas.
 sys.path.append(r'C:\Users\Pablo\Documents\GitHub\bank_CHURN')
 import pandas as pd
-from src.visualization_analysis.analysis import dumm_var
+from src.visualization_analysis.analysis import dumm_var, num_var, asymmetry, outliers_detector
 data = pd.read_csv('https://raw.githubusercontent.com/psochando/bank_CHURN/main/DATA/raw/Churn_Modelling.csv')
 
 
-def outliers_dealer(df):
 
-    indices_outliers = df[df['Age'] >= 63].index.values
-    df['Age'].iloc[indices_outliers] = df['Age'].median()
-
-    df['NumOfProducts'] = df['NumOfProducts'].replace(4, df['NumOfProducts'].median())
-    
+# Automatiza el tratamiento de outliers para características numéricas, imputando por la mediana cuando la asimetria de la distribucion es "grande" y por la media cuando no
+# Devuelve el dataframe actualizado
+def outliers_dealer(df = data, asym = 0.5):
+    a = asymmetry(df)
+    for col in num_var(df.drop(columns = 'Exited')):
+        outliers_indices = outliers_detector(df)[col].index
+        if abs(a[col]) > asym:
+            df[col].iloc[outliers_indices] = df[col].median()
+        else:
+            df[col].iloc[outliers_indices] = df[col].mean()
     return df
+            
 
-
+# Función principal en el procesamiento de datasets
 def process_data(drop_cols, target, df = data, split_X_y = False):
 
-    df.drop(columns = drop_cols, inplace = True)
+    df = df.drop(columns = drop_cols)
     # df = df.drop_duplicates() # se pierden casi 3000 registros si hacemos drop_duplicates despues de eliminar las columnas consideradas. Se ha decidido mantener estos registros repetidos, pues no se repiten por error sino que realmente dichos clientes presentan estos valores en las variables finalmente seleccionadas.
     
+    df = outliers_dealer(df) # tratamos los outliers en las variables numéricas del dataframe
+    
     cols_to_dummies = dumm_var(df)
-    df_dumm = pd.get_dummies(df[cols_to_dummies], drop_first = True)
+    df_dumm = pd.get_dummies(df[cols_to_dummies], drop_first = True) # codificamos las columnas categóricas
     df_not_dumm = df.drop(columns = cols_to_dummies)
     df = pd.concat([df_not_dumm, df_dumm], axis = 1)
     
-    df = outliers_dealer(df)
-    
-    if split_X_y == True:        
+    if split_X_y == True:  # posibilidad de separar en datos en variables predictoras y target
         X = df.drop(columns = target)
         X.to_csv(r'C:\Users\Pablo\Documents\GitHub\bank_CHURN\DATA\processed\dataset_completo_predictoras.csv', index=False)
         y = df[target]
@@ -38,4 +43,4 @@ def process_data(drop_cols, target, df = data, split_X_y = False):
 
 
 drop_cols = ['RowNumber', 'CustomerId', 'Surname', 'CreditScore', 'HasCrCard', 'EstimatedSalary', 'Tenure']
-process_data(drop_cols, 'Exited', split_X_y = True)
+process_data(drop_cols, 'Exited', split_X_y = False)
--- src/visualization_analysis/analysis.py
+++ src/visualization_analysis/analysis.py
@@ -3,7 +3,7 @@ import seaborn as sns
 from scipy.stats import skew
 data = pd.read_csv('https://raw.githubusercontent.com/psochando/bank_CHURN/main/DATA/raw/Churn_Modelling.csv')
 
-
+# Selecciona las variables numéricas del dataset
 def num_var(df = data):
     r = []
     for col in df.columns:
@@ -11,6 +11,7 @@ def num_var(df = data):
             r.append(col)
     return r
 
+# Selecciona las variables que nos interesan para aplicar one-hot encoding
 def dumm_var(df = data):
     r = []
     for col in df.columns:
@@ -19,21 +20,28 @@ def dumm_var(df = data):
     return r
     
 
-def outliers_detector(df = data[num_var()]):
-    
-    for col in df.columns:
-        
-        # Coeficiente de asimetría de Fisher
-        asymmetry = skew(df[col], bias=False)
-        print(f'Coef. de asimetría en {col}: {asymmetry}')
-        
-        # Outliers
+# Outliers de variables numéricas aplicando rangos intercuartílicos
+# Devuelve un diccionario con los outliers para cada variable
+def outliers_detector(df):
+    df_num = df[num_var(df)]
+    r = {}
+    for col in df_num.columns:
         q1 = df[col].quantile(0.25)
         q3 = df[col].quantile(0.75)
         iqr = q3 - q1
         lower_bound = q1 - 1.5 * iqr
         upper_bound = q3 + 1.5 * iqr
         outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)][col]
-        print(f'Outliers en {col}: {outliers.sort_values(ascending=True).values}')
-        print('Total:', len(outliers), '\n')
+        r[col] = outliers
+    return r
         
+        
+# Coeficientes de asimetría de Fisher
+# Devuelve un diccionario con los coeficientes de cada distribución    
+def asymmetry(df):
+    df_num = df[num_var(df)]
+    r = {}
+    for col in df_num.columns:
+        asymmetry = skew(df[col], bias=False)
+        r[col] = asymmetry
+    return r
